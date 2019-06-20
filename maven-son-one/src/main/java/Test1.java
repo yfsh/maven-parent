da5aa032046e72285f9a25b6bd2290315bf636cc
yfsh <136050581@qq.com>
2019-06-20 10:16:03 +0800

    Initial commit

---
+++ maven-son-one/src/main/java/Test1.java
@@ -0,0 +1,5 @@
+public class Test1 {
+    public void test1(){
+        System.out.println("test1");
+    }
+}
---
+++ maven-son-two/src/main/java/Test2.java
@@ -0,0 +1,5 @@
+public class Test2 {
+    public void test1(){
+        System.out.println("test2");
+    }
+}
